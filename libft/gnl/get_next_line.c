c77c58ec29856ef4cc3b313fdbd121b85ce516d5
jciglesias <jiglesia@student.42.fr>
2021-05-16 16:15:10 +0200

    gnl fixed

--- libft/gnl/get_next_line.c
+++ libft/gnl/get_next_line.c
@@ -6,52 +6,65 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 13:43:51 by jiglesia          #+#    #+#             */
-/*   Updated: 2021/05/08 23:06:08 by jiglesia         ###   ########.fr       */
+/*   Updated: 2021/05/16 16:12:04 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	fill_bowl(char **bowl, char *spoon)
+char	*fill_bowl(char *bowl, char *spoon)
 {
 	char	*dup;
 	int		i;
 	int		j;
 
-	dup = (char *)malloc(sizeof(char) * (ft_strlen(*bowl) + BUFFER_SIZE + 1));
-	if (!dup)
-		return ;
 	i = 0;
-	while (*bowl && (*bowl)[i])
+	dup = bowl;
+	bowl = (char *)malloc(ft_strlen(dup) + BUFFER_SIZE + 1);
+	if (!bowl)
+		return (ft_strdup(""));
+	while (dup && dup[i])
 	{
-		dup[i] = (*bowl)[i];
+		bowl[i] = dup[i];
 		i++;
 	}
-	if (bowl)
-		free(*bowl);
-	(*bowl) = dup;
+	bowl[i] = 0;
+	if (dup)
+		free(dup);
 	j = 0;
 	while (spoon[j])
 	{
-		(*bowl)[i + j] = spoon[j];
+		bowl[i + j] = spoon[j];
 		j++;
 	}
+	return (bowl);
+}
+
+int	check_jump(char *bowl)
+{
+	int	i;
+
+	i = 0;
+	while (bowl[i])
+		if (bowl[i++] == '\n')
+			return (0);
+	return (1);
 }
 
 void	scrap_bowl(char **bowl, int i)
 {
 	char *dup;
 
-	if ((*bowl)[i])
+	if ((*bowl)[i] && (*bowl)[i + 1])
 	{
-		dup = ft_strdup(&(*bowl)[i]);
+		dup = ft_strdup(&(*bowl)[i + 1]);
 		free(*bowl);
 		(*bowl) = dup;
 	}
 	else
 	{
 		free(*bowl);
-		(*bowl) = NULL;
+		(*bowl) = ft_strdup("");
 	}
 }
 
@@ -64,15 +77,14 @@ int	fill_line(char **bowl, char **line)
 	if (!(*line))
 		return (-1);
 	(*line)[i] = 0;
-	while ((*bowl)[i] && (*bowl)[i] != '\n')
+	while (*bowl && (*bowl)[i] && (*bowl)[i] != '\n')
 	{
 		(*line)[i] = (*bowl)[i];
 		(*line)[++i] = 0;
 	}
-	write(1, "hi\n", 3);
-	if ((*bowl)[i])
+	if (*bowl && (*bowl)[i])
 	{
-		scrap_bowl(bowl, i + 1);
+		scrap_bowl(bowl, i);
 		return (1);
 	}
 	else
@@ -90,13 +102,17 @@ int	get_next_line(int fd, char **line)
 	static char		*bowl = NULL;
 	size_t			j;
 
+	if (!bowl)
+		bowl = ft_strdup("");
 	if (fd >= 0 && line && !(read(fd, spoon, 0)))
 	{
 		j = read(fd, spoon, BUFFER_SIZE);
 		while (j > 0)
 		{
 			spoon[j] = 0;
-			fill_bowl(&bowl, spoon);
+			bowl = fill_bowl(bowl, spoon);
+			if (!check_jump(bowl))
+				break ;
 			j = read(fd, spoon, BUFFER_SIZE);
 		}
 		return (fill_line(&bowl, line));
