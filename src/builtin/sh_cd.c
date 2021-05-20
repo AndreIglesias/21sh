5b51e846a2c92bbb3a305e90cd0b7ced51c6cb27
jciglesias <jiglesia@student.42.fr>
2021-05-20 14:25:05 +0200

    change ~ to HOME

--- src/builtin/sh_cd.c
+++ src/builtin/sh_cd.c
@@ -6,18 +6,36 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 13:04:58 by jiglesia          #+#    #+#             */
-/*   Updated: 2021/05/20 13:22:34 by jiglesia         ###   ########.fr       */
+/*   Updated: 2021/05/20 14:23:32 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "msh.h"
 
+char	*get_home_dir(t_trie *ev, char *path)
+{
+	char	*tmp;
+
+	tmp = get_value(ev, "HOME");
+	if (tmp)
+	{
+		tmp = ft_strjoin(tmp, &path[1]);
+		return (tmp);
+	}
+	return (NULL);
+}
+
 int	sh_cd(t_trie *ev, char *path)
 {
 	char	*tmp;
 
-	if (!chdir(path))
+	tmp = ft_strdup(path);
+	if (path && path[0] == '~')
+		tmp = get_home_dir(ev, path);
+	if (!chdir(tmp))
 	{
+		if (tmp)
+			free(tmp);
 		tmp = getcwd(NULL, 0);
 		sh_export(ev, "PWD", tmp);
 		free(tmp);
