65c6878d54328c3ea9fc658484e0f11c385993a3
AndreIglesias <ciglesia@student.42.fr>
2021-05-31 20:07:11 +0200

    syntax macro

--- include/msh.h
+++ include/msh.h
@@ -6,18 +6,18 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:04:26 by ciglesia          #+#    #+#             */
-/*   Updated: 2021/05/31 19:51:55 by user             ###   ########.fr       */
+/*   Updated: 2021/05/31 20:04:52 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSH_H
 # define MSH_H
 
-#ifdef M_SYNTAX
-# define SYNTAX 1
-#else
-# define SYNTAX 0
-#endif
+# ifdef M_SYNTAX
+#  define SYNTAX	1
+# else
+#  define SYNTAX	0
+# endif
 
 # include "libft.h"
 
