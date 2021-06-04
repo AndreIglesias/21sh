8079ae78e3f177937234837c223ab993e1364b9b
jciglesias <jiglesia@student.42.fr>
2021-06-04 13:52:08 +0200

    sh_export parsing and two format acceptance

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 18:29:37 by jiglesia          #+#    #+#             */
-/*   Updated: 2021/06/02 23:38:26 by user             ###   ########.fr       */
+/*   Updated: 2021/06/04 01:06:01 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,5 +114,6 @@ void				ft_freesplit(char **split);
 void				ft_puttrie(t_trie *root, char *str, int lvl);
 void				ft_freetrie(t_trie **root);
 long long			ft_atoll(char *nb);
+int					ft_counttil(const char *s, char c);
 
 #endif
